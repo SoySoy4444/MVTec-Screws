df1d0e7415853134367973fb8349d30a64f8c2bd
ss8622 <soya.shinkura22@imperial.ac.uk>
2023-09-06 21:53:01 +0900

    Deleted redundant cnn versions, created final version

--- cnn.py
+++ cnn.py
@@ -2,66 +2,120 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
 from keras.losses import binary_crossentropy
-from keras.optimizers import SGD
+from keras.optimizers.legacy import SGD, Adam # for M1/M2 macs, legacy needed
+from keras.metrics import Precision, Recall, AUC, F1Score
+from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay, roc_curve
+from preprocessor import loadDir
+from image_augmentation import augmentImages
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import shutil
+import glob
+import random
 from keras import backend as K
+import visualkeras
+import zipfile
 
-TRAIN_PATH = "archive/train/"
-# TRAIN_PATH = "preprocessed/"
+np.random.seed(42)
 BATCH_SIZE = 20
+# MODE can be "augmented", "preprocessed" or "archive"
+MODE = "augmented"
+TRAIN_PATH = f"{MODE}/train/"
+VALIDATION_PATH = f"{MODE}/validation"
+TESTING_PATH = f"{MODE}/testing"
+NUM_EPOCHS = 10
+LR = 0.0002
+
+# clear the directory
+if os.path.exists(MODE):
+    shutil.rmtree(MODE)
+
+# add it back
+if MODE == "archive":
+    with zipfile.ZipFile("screws.zip", 'r') as dir:
+        dir.extractall()
+elif MODE == "preprocessed":
+    loadDir("archive/train")
+else:
+    augmentImages()
+
+# create necessary directories
+directories = [f"{MODE}/validation/good", f"{MODE}/validation/not-good", f"{MODE}/testing/good", f"{MODE}/testing/not-good"]
+for dir in directories:
+    os.makedirs(dir)
+
+# move 10% of the data from training into each of testing and validation
+for c in random.sample(glob.glob(f"{MODE}/train/good/*.png"), 25):
+    shutil.move(c, f"{MODE}/validation/good")
+
+for c in random.sample(glob.glob(f"{MODE}/train/not-good/*.png"), 5):
+    shutil.move(c, f"{MODE}/validation/not-good")
+
+for c in random.sample(glob.glob(f"{MODE}/train/good/*.png"), 25):
+    shutil.move(c, f"{MODE}/testing/good")
+
+for c in random.sample(glob.glob(f"{MODE}/train/not-good/*.png"), 5):
+    shutil.move(c, f"{MODE}/testing/not-good")
 
 # greyscale image, so only 1 channel
 img_shape = (224, 224, 1) if K.image_data_format() == "channels_last" else (1, 224, 224)
 
 model = Sequential()
-model.add(Conv2D(16, kernel_size=(3, 3), activation="relu", input_shape=img_shape, padding="same")) # 224x224 grey-scale images
+model.add(Conv2D(8, kernel_size=(3, 3), activation="relu", input_shape=img_shape, padding="same")) # 224x224 grey-scale images
 
-model.add(Conv2D(32, (3, 3), activation="relu", padding='same'))
+model.add(Conv2D(16, (3, 3), activation="relu", padding='same'))
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(32, (3, 3), activation="relu", padding='same'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), activation="relu", padding='same'))
+model.add(Conv2D(20, (3, 3), activation="relu", padding='same'))
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(64, activation="relu"))
+model.add(Dense(32, activation="relu"))
 model.add(Dropout(0.5))
-model.add(Dense(1, activation="sigmoid")) # binary classification hence 1 & sigmoid
+model.add(Dense(1, activation="sigmoid")) # binary classification hence 1 class & sigmoid
 
-model.compile(loss=binary_crossentropy, optimizer=SGD(learning_rate=0.0002), metrics=["accuracy"])
+# model.compile(loss=binary_crossentropy, optimizer=SGD(learning_rate=LR), metrics=["accuracy"])
+model.compile(loss=binary_crossentropy, optimizer=Adam(learning_rate=LR), metrics=["accuracy", Precision(), Recall(), F1Score(), AUC()])
 
+# Get summary of model and visualise the layers
 model.summary()
+visualkeras.layered_view(model, to_file="architecture.png") # .show()
 
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.1,
     zoom_range=0.1,
     horizontal_flip=True,
-    validation_split=0.2
+    vertical_flip=True
 )
+validation_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     TRAIN_PATH,
     target_size=(224, 224),
     batch_size=BATCH_SIZE,
     class_mode="binary",
-    subset="training",
-    save_format="jpeg",
-    save_to_dir="generated/train",
-    color_mode="grayscale"
+    color_mode="grayscale",
+    # save_format="jpeg",
+    # save_to_dir="generated/train"
 )
 
-validation_generator = train_datagen.flow_from_directory(
-    TRAIN_PATH,
+validation_generator = validation_datagen.flow_from_directory(
+    VALIDATION_PATH,
     target_size=(224, 224),
     batch_size=BATCH_SIZE,
     class_mode='binary',
-    subset='validation',
+    color_mode="grayscale"
+)
+
+testing_generator = validation_datagen.flow_from_directory(
+    TESTING_PATH,
+    target_size=(224,224),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
     color_mode="grayscale"
 )
 
@@ -73,7 +127,6 @@ print(f"Validation generator samples = {validation_generator.samples}")
 mapping = train_generator.class_indices  # {'good': 0, 'not-good': 1}
 # 250 good samples and 50 not-good samples so ratio is 0.83:0.17
 class_weights = {mapping["not-good"] : 0.17, mapping["good"] : 0.83}
-NUM_EPOCHS = 10
 hist = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples//BATCH_SIZE,
@@ -91,6 +144,37 @@ from datetime import datetime
 now = datetime.now()
 model.save(f"models/{now.strftime('%Y_%m_%d_%H%M')}_{NUM_EPOCHS}_epochs.keras")
 
-# Evaluation stuff
-# need to put test_generator
-print(model.evaluate(train_generator)) # loss value and metrics values
+# Evaluation
+categories = ["good", "not-good"] # order determined by .class_indices
+print(model.evaluate(testing_generator)) # loss value and metrics values
+y_pred = model.predict(testing_generator)
+y_pred = np.argmax(y_pred, axis=1)
+cm = confusion_matrix(testing_generator.classes, y_pred, normalize='pred')
+print(cm)
+print(classification_report(validation_generator.classes, y_pred, target_names=categories))
+
+matrix = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=categories)
+matrix.plot(cmap=plt.cm.Blues)
+plt.savefig("confusion_matrix.png")
+plt.show()
+
+# validation and train graphs
+plt.figure()
+plt.plot(hist.history["loss"], label="train")
+plt.plot(hist.history["val_loss"], label="validation")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Loss & Validation Loss")
+plt.legend()
+plt.savefig("loss_graph.png")
+plt.show()
+
+fpr, tpr, thresholds = roc_curve(validation_generator.classes, y_pred)
+plt.figure(1)
+plt.plot([0, 1], [0, 1], 'y--')
+plt.plot(fpr, tpr, marker='.')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.savefig("roc_graph.png")
+plt.show()
