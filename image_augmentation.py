a0f50f5dea9429910f85ae1dd0e6a02e70a776fc
ss8622 <soya.shinkura22@imperial.ac.uk>
2023-09-06 21:54:20 +0900

    Placed image augmentation tools inside a function

--- image_augmentation.py
+++ image_augmentation.py
@@ -1,37 +1,37 @@
 import os
 from keras.preprocessing.image import ImageDataGenerator
 
-datagen = ImageDataGenerator(
-    rotation_range=45,
-    width_shift_range=0,  # % shift
-    height_shift_range=0,
-    shear_range=0.05,
-    zoom_range=0.05,
-    horizontal_flip=True,
-    fill_mode='nearest', # constant, nearest, reflect or warp
-) 
+def augmentImages():
+    datagen = ImageDataGenerator(
+        rotation_range=45,
+        width_shift_range=0,  # % shift
+        height_shift_range=0,
+        shear_range=0.05,
+        zoom_range=0.05,
+        horizontal_flip=True,
+        fill_mode='reflect', # constant, nearest, reflect or warp
+    ) 
+    if not os.path.exists("augmented/train/good"):
+        print("Created new directory: augmented/train/good")
+        os.makedirs("augmented/train/good")
+    if not os.path.exists("augmented/train/not-good"):
+        print("Created new directory: augmented/train/not-good")
+        os.makedirs("augmented/train/not-good")
 
-if not os.path.exists("augmented"):
-    print("Created new directory: augmented")
-    os.makedirs("augmented")
-if not os.path.exists("augmented/good"):
-    print("Created new directory: augmented/good")
-    os.makedirs("augmented/good")
-if not os.path.exists("augmented/not-good"):
-    print("Created new directory: augmented/not-good")
-    os.makedirs("augmented/not-good")
+    for category in ["good", "not-good"]:
+        i = 0
+        print(f'archive/train/{category}/')
+        for batch in datagen.flow_from_directory(directory=f'archive/train/',
+                                                batch_size=16,
+                                                target_size=(256, 256),
+                                                color_mode="grayscale",
+                                                classes=[category],
+                                                save_to_dir=f'augmented/train/{category}/',
+                                                save_prefix='aug',
+                                                save_format='png'):
+            i += 1
+            if i > 31:
+                break
 
-for category in ["good", "not-good"]:
-    i = 0
-    print(f'archive/train/{category}/')
-    for batch in datagen.flow_from_directory(directory=f'archive/train/',
-                                            batch_size=16,
-                                            target_size=(256, 256),
-                                            color_mode="grayscale",
-                                            classes=[category],
-                                            save_to_dir=f'augmented/{category}/',
-                                            save_prefix='aug',
-                                            save_format='png'):
-        i += 1
-        if i > 31:
-            break
+if __name__ == "__main__":
+    augmentImages()
